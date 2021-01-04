fd703b45561b3c4d81861954d246440dfd8fd020
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-03 23:07:47 -0300

    add smoke tests

---
+++ tests/main.spec.js
@@ -0,0 +1,29 @@
+import { expect } from 'chai';
+
+import {
+  search, searchAlbums, searchArtists, searchTracks, searchPlaylists,
+} from '../src/main';
+
+describe('Spotify Wrapper', () => {
+  describe('Smoke Tests', () => {
+    it('Should exists the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('Should exists the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('Should exists the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('Should exists the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('Should exists the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+});
