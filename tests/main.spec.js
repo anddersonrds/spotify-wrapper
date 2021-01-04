a539e98c2e64f5ffc3f8e3886ea412e04218aa2a
Anderson Rodrigues <anddersonrds@gmail.com>
2021-01-04 09:35:37 -0300

    add test using calledOnce & calledWith

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,36 +1,60 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
 
 import {
   search, searchAlbums, searchArtists, searchTracks, searchPlaylists,
 } from '../src/main';
 
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
+
 describe('Spotify Wrapper', () => {
   describe('Smoke Tests', () => {
-    it('Should exists the search method', () => {
+    it('should exists the search method', () => {
       expect(search).to.exist;
     });
 
-    it('Should exists the searchAlbums method', () => {
+    it('should exists the searchAlbums method', () => {
       expect(searchAlbums).to.exist;
     });
 
-    it('Should exists the searchArtists method', () => {
+    it('should exists the searchArtists method', () => {
       expect(searchArtists).to.exist;
     });
 
-    it('Should exists the searchTracks method', () => {
+    it('should exists the searchTracks method', () => {
       expect(searchTracks).to.exist;
     });
 
-    it('Should exists the searchPlaylists method', () => {
+    it('should exists the searchPlaylists method', () => {
       expect(searchPlaylists).to.exist;
     });
   });
 
   describe('Generic Search', () => {
-    it('Should call fetch function', () => {
+    it('should call fetch function', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
       const artists = search();
-      console.log(artists);
+
+      expect(fetchedStub).to.have.been.calledOnce;
+
+      fetchedStub.restore();
+    });
+
+    it('should receive the correct url to fetch', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search('Incubus', 'artist');
+
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+      const albums = search('Incubus', 'album');
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
     });
   });
 });
